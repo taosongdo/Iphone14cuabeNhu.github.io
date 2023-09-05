2b7595b948da1460c67b62610111096c9b82adab
Tuan <lygiatuan200804@gmail.com>
2023-09-05 19:17:04 +0700

    mfff

--- iphone14.js
+++ iphone14.js
@@ -10,38 +10,44 @@ function set2(z){
     z.css({
         "width":"30%",
         "border-radius":"30px",
-        "height":"20px"
+        "height":"20px",
+        "right":"78.5px"
     }) 
     z.html(``);
 }
 window.onload = function(){
     let a = document.querySelector("div.cum");
-    $(a).children("div.dynamic").addEventListener("click", function(){
-        if(Math.round(parseFloat($(a).children(".dynamic").children("div").css("height"))) !== 357)
-        {
-            $(a).children(".dynamic").children("div").html(
-            `
-                <div class="dynamicthem">
-                    <div class="info"><p>Iphone 14 Pro max của bé Như</p></div>
-                    <div class="hinh"><img src="hinhmeo.jpg"/></div>
-                </div>  
-            `)
-            $(a).children(".dynamic").children("div").css({
-                "width":"213.8px",
-                "border-radius":"25px",  
-                "height":"357px"
-            });
-            setTimeout(set1,500,$(a).children(".dynamic").children("div").children("div.dynamicthem").children("div.info"))
-        }
-        else
-        {
-            $(a).children(".dynamic").children("div").children("div.dynamicthem").children("div.info").css({
-                "opacity":"0",
-            });
-            $(a).children(".dynamic").children("div").children("div.dynamicthem").children("div.hinh").css({
-                "opacity":"0",
-            })
-            setTimeout(set2,500,$(a).children(".dynamic").children("div"));
-        }
-    });
+    let b =document.querySelectorAll("div.cum > div");
+    for(c of b)
+    {
+        c.addEventListener("click", function(){
+            if(Math.round(parseFloat($(a).children(".dynamic").css("height"))) !== 357)
+            {
+                $(a).children(".dynamic").html(
+                `
+                    <div class="dynamicthem">
+                        <div class="info"><p>Iphone 14 Pro max của bé Như</p></div>
+                        <div class="hinh"><img src="hinhmeo.jpg"/></div>
+                    </div>  
+                `)
+                $(a).children(".dynamic").css({
+                    "width":"213.8px",
+                    "border-radius":"25px",  
+                    "height":"357px",
+                    "right":"4.5px"
+                });
+                setTimeout(set1,500,$(a).children(".dynamic").children("div.dynamicthem").children("div.info"))
+            }
+            else
+            {
+                $(a).children(".dynamic").children("div.dynamicthem").children("div.info").css({
+                    "opacity":"0",
+                });
+                $(a).children(".dynamic").children("div.dynamicthem").children("div.hinh").css({
+                    "opacity":"0",
+                })
+                setTimeout(set2,500,$(a).children(".dynamic"));
+            }
+        });
+    }
 }
